87f4aedc8627d642551bf37d6a6a67cd652dcb3b
Emilse Arias <emilsearias@gmail.com>
2024-03-04 20:25:34 -0300

    agregamos la logica de eliminar un nodo del DOM

--- js/app.js
+++ js/app.js
@@ -13,6 +13,9 @@ function modificarTitulo() {
 }
 
 function crearElementoNuevo() {
+  //3- buscar nodo padre
+  const seccionElementos = document.querySelector("#seccionElementos");
+
   if (botonVerMas.className === "btn btn-outline-light") {
     //crear el parrafo
     //crear un elemento con el objeto document
@@ -21,8 +24,6 @@ function crearElementoNuevo() {
     //2 - modificar las propiedades
     parrafoNuevo.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere praesentium repellendus excepturi explicabo temporibus magni molestiae ea non ipsa ut porro, modi sequi pariatur maiores eveniet dolore! A modi voluptas repellendus amet quasi architecto. Praesentium quam nihil, iure laboriosam blanditiis, molestias eum sit esse corporis, dicta fugiat. Odit quaerat iure ipsam fugit illum cupiditate sapiente, possimus optio esse. Hic dolorum eum temporibus, accusamus omnis optio sit in, beatae veniam commodi quo! Deserunt saepe voluptatem officia, soluta error commodi praesentium quibusdam neque laboriosam corrupti! Nesciunt repudiandae tempore expedita facere obcaecati ipsa minus natus velit officia hic? Officiis expedita facere tempore deserunt ab obcaecati rerum odio ipsum ducimus voluptatibus praesentium eos provident aperiam, soluta ratione sequi enim? Perspiciatis tempore ipsum quaerat laboriosam amet incidunt dolore aliquid iure assumenda nihil? Necessitatibus modi deserunt laboriosam iure, itaque nisi? Repellat dignissimos dolorum delectus corporis exercitationem labore iusto quos error reiciendis ad modi deleniti quidem, ex fugiat officiis dolorem saepe quod nisi? Excepturi quidem dignissimos tempora suscipit animi adipisci ipsa omnis nesciunt similique laudantium, possimus sint, eaque ad, magni voluptatem consectetur. Aperiam, culpa! Vitae quaerat saepe aut quae illum, voluptates soluta voluptatem dolorum aspernatur dolore vel unde sint totam fuga ipsam quos ratione illo ab doloremque!`;
     parrafoNuevo.className = "lead";
-    //3- buscar nodo padre
-    const seccionElementos = document.querySelector("#seccionElementos");
     // seccionElementos.appendChild(parrafoNuevo); //agrega nodos hijos al final
     // seccionElementos.prepend(parrafoNuevo); //agrega nodos hijos al principio
     seccionElementos.insertBefore(parrafoNuevo, botonVerMas);
@@ -32,7 +33,11 @@ function crearElementoNuevo() {
     botonVerMas.classList.add("btnVerMas");
     // botonVerMas.className = 'btn btnVerMas'
   } else {
-    console.log(seccionElementos.children[2])
+    console.log(seccionElementos.children[2]);
+    seccionElementos.removeChild(seccionElementos.children[2]);
+    //resetear el botón
+    botonVerMas.className = 'btn btn-outline-light';
+    botonVerMas.innerHTML = 'Ver mas...'
   }
   //   console.log(botonVerMas.className);
   //   console.log(botonVerMas.classList.contains("btn-outline-light"));
